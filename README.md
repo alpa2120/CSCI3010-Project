ce8065016120fa02e12838972156ceafc0b95a8f
Alex Palo <alexander.palo@colorado.edu>
2020-02-23 19:37:05 -0700

    Change app to scss

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# CSCI3010Project
+# Project
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('csci3010-project App', () => {
+describe('project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
